8ba21cd2475b2cf260eba65b1b89753850caaffa
AndersonHJB <cleland1432803776@icloud.com>
2023-06-18 01:49:55 +0800

    first commit

---
+++ NoteBook/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/6/18 01:44
+# @Author  : AI悦创
+# @FileName: __init__.py.py
+# @Software: PyCharm
+# @Blog    ：https://bornforthis.cn/
+from engine import *
---
+++ NoteBook/engine.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/6/18 01:46
+# @Author  : AI悦创
+# @FileName: engine.py
+# @Software: PyCharm
+# @Blog    ：https://bornforthis.cn/
