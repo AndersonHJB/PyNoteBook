9b21b105715b6b2bedcfdba9c06195bfc05c1993
AndersonHJB <cleland1432803776@icloud.com>
2023-06-18 02:05:34 +0800

    update

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from shutil import rmtree
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = 'PyNoteBook'
+NAME = 'PyNote'
 DESCRIPTION = '方便学员查询 Python 相关知识点,辅助考试。'
 URL = 'https://github.com/me/myproject'
 EMAIL = 'bornforthis@bornforthis.com'
